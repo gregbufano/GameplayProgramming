7b8444d589408c57a179656b458c621e16ac66cd
gregbufano <gregorybufano@aol.com>
2025-12-20 14:23:37 -0700

    Setup logic for death widget and currently testing victory widget

---
+++ Source/GameplayProgramming/Public/VictoryTriggerVolume.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UATPickup.h"
+#include "VictoryTriggerVolume.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPLAYPROGRAMMING_API AVictoryTriggerVolume : public AUATPickup
+{
+	GENERATED_BODY()
+	
+};
